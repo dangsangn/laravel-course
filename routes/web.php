62b782ba9576d995644dcbe3e1c016113e4546ff
dangsangn <dangsangn6147@gmail.com>
2025-06-24 23:56:33 +0000

    router laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return view('about');
+});
+
+Route::get('/sum/{a}/{b}', function(float $a, float $b) {
+    return $a + $b;
+})->whereNumber(['a', 'b']);
